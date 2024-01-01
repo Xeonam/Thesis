730cf796a269f1db95c4bb3e12def4042278e82c
Balázs Sidlóczki <sidbalazs18@gmail.com>
2024-01-01 11:58:26 +0100

    refactor: typos in schemas.py

--- schema/schemas.py
+++ schema/schemas.py
@@ -1,26 +1,26 @@
 from marshmallow import Schema, fields
 
 class UserSchema(Schema):
-    id = fields.Int(dump_only=True)
+    id = fields.Integer(dump_only=True)
     username = fields.String()
     email = fields.String()
     pw = fields.String()
 
 class WordSchema(Schema):
-    id = fields.Int(dump_only=True)
+    id = fields.Integer(dump_only=True)
     english_word = fields.String()
     hungarian_meaning = fields.String()
 
 class CardSchema(Schema):
-    id = fields.Int(dump_only=True)
-    user_id = fields.Int()
-    word_id = fields.Int()
+    id = fields.Integer(dump_only=True)
+    user_id = fields.Integer()
+    word_id = fields.Integer()
     due = fields.DateTime()
     stability = fields.Float()
     difficulty = fields.Float()
-    elapsed_days = fields.Int()
-    scheduled_days = fields.Int()
-    reps = fields.Int()
-    lapses = fields.Int()
-    state = fields.Int()
+    elapsed_days = fields.Integer()
+    scheduled_days = fields.Integer()
+    reps = fields.Integer()
+    lapses = fields.Integer()
+    state = fields.Integer()
     last_review = fields.DateTime()
