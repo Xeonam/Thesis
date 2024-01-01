6306e7772634fb187da94b45ef039f5f04d72762
Balázs Sidlóczki <sidbalazs18@gmail.com>
2024-01-01 11:20:11 +0100

    feat: add word model

--- model/models.py
+++ model/models.py
@@ -8,3 +8,9 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     pw = db.Column(db.String(255), nullable=False)
     cards = db.relationship('Card', back_populates='user', lazy=True)
+
+class Word(db.Model):
+    word_id = db.Column(db.Integer, primary_key=True)
+    english_word = db.Column(db.String(255), nullable=False)
+    hungarian_meaning = db.Column(db.String(255), nullable=False)
+    cards = db.relationship('Card', back_populates='word', lazy=True)
