92f2e9d5947d159b299dca56bec169ce94cc4a9c
Balázs Sidlóczki <sidbalazs18@gmail.com>
2024-01-01 11:27:28 +0100

    feat: add card model

--- model/models.py
+++ model/models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from fsrs import FSRS, Rating
 
 db = SQLAlchemy()
 
@@ -14,3 +16,38 @@ class Word(db.Model):
     english_word = db.Column(db.String(255), nullable=False)
     hungarian_meaning = db.Column(db.String(255), nullable=False)
     cards = db.relationship('Card', back_populates='word', lazy=True)
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    word_id = db.Column(db.Integer, db.ForeignKey('word.word_id'), nullable=False)
+    due = db.Column(db.DateTime, default=datetime.utcnow)
+    stability = db.Column(db.Float, default=0)
+    difficulty = db.Column(db.Float, default=0)
+    elapsed_days = db.Column(db.Integer, default=0)
+    scheduled_days = db.Column(db.Integer, default=0)
+    reps = db.Column(db.Integer, default=0)
+    lapses = db.Column(db.Integer, default=0)
+    state = db.Column(db.Integer, default=0)
+    last_review = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship('User', back_populates='cards')
+    word = db.relationship('Word', back_populates='cards')
+
+
+    def do_repeat(self, user_rating: Rating) -> None:
+        now = datetime.utcnow()
+        scheduling_card = FSRS().repeat(self, now)
+
+        scheduling_card_user_rating = scheduling_card[user_rating].card
+
+        self.last_review = now
+        self.due = scheduling_card_user_rating.due
+        self.stability = scheduling_card_user_rating.stability
+        self.difficulty = scheduling_card_user_rating.difficulty
+        self.elapsed_days = scheduling_card_user_rating.elapsed_days
+        self.scheduled_days = scheduling_card_user_rating.scheduled_days
+        self.reps = scheduling_card_user_rating.reps
+        self.lapses = scheduling_card_user_rating.lapses
+        self.state = scheduling_card_user_rating.state
+        self.last_review = scheduling_card_user_rating.last_review
