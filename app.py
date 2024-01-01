25be4be121c61318956cfc26c35898b8b58939dd
Balázs Sidlóczki <sidbalazs18@gmail.com>
2024-01-01 11:43:20 +0100

    feat: connect app with db

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from model.models import db
+from instance.config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
 
 @app.route('/')
 def index():
