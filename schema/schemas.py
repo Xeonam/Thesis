ec7226f195024b6a4146b1bf214ddad8968733ae
Balázs Sidlóczki <sidbalazs18@gmail.com>
2024-01-01 11:56:08 +0100

    feat: add CardSchema

--- schema/schemas.py
+++ schema/schemas.py
@@ -10,3 +10,17 @@ class WordSchema(Schema):
     id = fields.Int(dump_only=True)
     english_word = fields.String()
     hungarian_meaning = fields.String()
+
+class CardSchema(Schema):
+    id = fields.Int(dump_only=True)
+    user_id = fields.Int()
+    word_id = fields.Int()
+    due = fields.DateTime()
+    stability = fields.Float()
+    difficulty = fields.Float()
+    elapsed_days = fields.Int()
+    scheduled_days = fields.Int()
+    reps = fields.Int()
+    lapses = fields.Int()
+    state = fields.Int()
+    last_review = fields.DateTime()
