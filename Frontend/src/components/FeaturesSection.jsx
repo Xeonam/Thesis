915799f5624ec5bd928dadb9c4a2d5aad9a1b419
Balázs Sidlóczki <sidbalazs18@gmail.com>
2024-01-04 16:59:29 +0100

    feat: add animation to feature cards

--- Frontend/src/components/FeaturesSection.jsx
+++ Frontend/src/components/FeaturesSection.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import translateIcon from "../assets/animations/translate_animation.json";
+import learnIcon from "../assets/animations/learn_animation.json";
+import signUpIcon from "../assets/animations/sign_up_animation.json";
+import Lottie from "lottie-react";
 
-const FeatureCard = ({ title, text, bgColor }) => (
+const FeatureCard = ({title, text, bgColor, animation }) => (
   <div
     className={`flex flex-col items-center p-4 ${bgColor} rounded-lg shadow`}
   >
+    <Lottie animationData={animation} className="w-1/2" />
     <h3 className="text-lg font-semibold">{title}</h3>
     <p className="text-sm text-center">{text}</p>
   </div>
@@ -24,6 +29,7 @@ const FeaturesSection = () => {
           text="Master English to Hungarian translations effortlessly with our free
             tool. Experience lightning-fast, reliable translations at no cost and
             bridge language gaps instantly."
+            animation={translateIcon}
         ></FeatureCard>
         <FeatureCard
           title="Effective Flashcard Learning"
@@ -31,6 +37,7 @@ const FeaturesSection = () => {
           text="Reinforce your language learning with targeted flashcards. Practice
           vocabulary and phrases with a system designed to boost retention and
           accelerate your learning journey."
+          animation={learnIcon}
         ></FeatureCard>
 
         <FeatureCard
@@ -39,6 +46,7 @@ const FeaturesSection = () => {
           text="Jump right into learning with our streamlined registration process. It
           takes only moments to sign up and start your path to language
           proficiency."
+          animation={signUpIcon}
         ></FeatureCard>
       </div>
     </section>
