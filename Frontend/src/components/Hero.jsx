e331b7f9445d62acca32bb2364598a27a512c1b2
Balázs Sidlóczki <sidbalazs18@gmail.com>
2024-01-04 19:05:10 +0100

    refactor: update text in Hero component

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -6,17 +6,21 @@ function Hero() {
       <div className="space-y-6 md:space-y-8">
         <p className="text-white text-center text-6xl">
           <span className="text-customTextHighlight">Elevate</span> your skills
-          using WordEnlighten!
+          using <span className="text-customNavbarText">WordEnlighten</span>!
         </p>
 
-        <p className="text-white text-center text-4xl">
+        <p className="text-white text-center text-4xl italic ">
           Simple, effective, and free
         </p>
 
         <p className="text-white text-center text-3xl">
-          <span className="text-customTextHighlight">Start</span> your journey to global fluency today. Join us and
-          transcend language barriers, one word at a time!
+          <span className="text-customTextHighlight">Start</span> your journey
+          to global fluency today. 
         </p>
+
+        <p className="text-white text-center text-3xl">
+          <span className="text-customTextHighlight">Join</span> us and transcend language barriers, one
+          word at a time!</p>
       </div>
     </div>
   );
