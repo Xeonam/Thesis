921f21f591506c9cda7eda2b59979bdb10055566
Balázs Sidlóczki <sidbalazs18@gmail.com>
2024-01-05 10:46:55 +0100

    feat: add sign-up form to SignUp component

--- Frontend/src/components/SignUp.jsx
+++ Frontend/src/components/SignUp.jsx
@@ -1,9 +1,114 @@
-import React from 'react'
+import React from "react";
 
 function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div>
+      <section className="bg-customNavbar">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
+          <a
+            href="/"
+            class="flex items-center mb-6 text-2xl font-semibold text-customNavbarText"
+          >
+            <h1>WordEnlighten</h1>
+          </a>
+          <div className="w-full bg-[#87CEFA] rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 ">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+                Create an account
+              </h1>
+              <form className="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label
+                    for="email"
+                    class="block mb-2 text-sm font-medium text-gray-900 "
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                    placeholder="name@company.com"
+                    required=""
+                  />
+                </div>
+                <div>
+                  <label
+                    for="password"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                    required=""
+                  />
+                </div>
+                <div>
+                  <label
+                    for="confirm-password"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
+                  >
+                    Confirm password
+                  </label>
+                  <input
+                    type="confirm-password"
+                    name="confirm-password"
+                    id="confirm-password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                    required=""
+                  />
+                </div>
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="terms"
+                      aria-describedby="terms"
+                      type="checkbox"
+                      class="w-4 h-4 border border-gray-300 rounded bg-gray-50"
+                      required=""
+                    />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label for="terms" className="font-light text-black-500 ">
+                      I accept the{" "}
+                      <a
+                        className="font-medium text-primary-600 hover:underline"
+                        href="#"
+                      >
+                        Terms and Conditions
+                      </a>
+                    </label>
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full text-white bg-customNavbar hover:bg-primary-700 active:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                >
+                  Create an account
+                </button>
+                <p className="text-sm font-light text-black-500">
+                  Already have an account?{" "}
+                  <a
+                    href="#"
+                    className="font-medium text-primary-600 hover:underline"
+                  >
+                    Login here
+                  </a>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default SignUp
+export default SignUp;
