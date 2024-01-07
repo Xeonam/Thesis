3b4e93c75d447a032c309d70cf656897b193ca6d
Balázs Sidlóczki <sidbalazs18@gmail.com>
2024-01-07 15:17:28 +0100

    feat: update text color in Words component

--- Frontend/src/components/Words.jsx
+++ Frontend/src/components/Words.jsx
@@ -18,7 +18,7 @@ function Words() {
 
   return (
     <div className="p-5 bg-[#412A4C]">
-      <h1 className="text-2xl font-bold mb-5">Words</h1>
+      <h1 className="text-2xl font-bold mb-5 text-importantText">Words</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-5">
         {data?.map((wordItem) => (
           <div
