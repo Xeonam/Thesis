521bd0dd772b2cbbb5fa9025ec31b2bfb653bf88
Balázs Sidlóczki <sidbalazs18@gmail.com>
2024-01-07 13:46:56 +0100

    refactor: refactor Words component

--- Frontend/src/components/Words.jsx
+++ Frontend/src/components/Words.jsx
@@ -1,38 +1,43 @@
-import React from 'react'
+import React from "react";
 import { useQuery } from "@tanstack/react-query";
-import { fetchWords } from '../api/apiCalls';
+import { fetchWords } from "../api/apiCalls";
 
 function Words() {
-    // fetch the words from the api
-    const { data, isLoading, error } = useQuery({
-        queryKey: ['words'],
-        queryFn: fetchWords
-    });
+  // fetch the words from the api
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["words"],
+    queryFn: fetchWords,
+  });
 
-    // Log the data to the console
-    console.log(data);
+  // Log the data to the console
+  console.log(data);
 
-    // Handle loading state
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
+  // Handle loading state
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-    // Handle error state
-    if (error) {
-        return <div>Error! {error.message}</div>;
-    }
+  // Handle error state
+  if (error) {
+    return <div>Error! {error.message}</div>;
+  }
 
-    // Render the data
-    return (
-        <div>
-            {data && (
-                <div>
-                    <h2>Words:</h2>
-                    <pre>{JSON.stringify(data, null, 2)}</pre>
-                </div>
-            )}
+  // Render the data
+  return (
+    <div>
+      <h1>Words</h1>
+      
+      <div>
+      { data ? data.map(wordItem => (
+        <div key={wordItem.card_id}>
+          <h3>English Word: {wordItem.word.english_word}</h3>
+          <p>Hungarian Meaning: {wordItem.word.hungarian_meaning}</p>
         </div>
-    );
+      )): null}
+    </div>
+
+    </div>
+  );
 }
 
 export default Words;
