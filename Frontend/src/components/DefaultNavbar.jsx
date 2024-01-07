9f7bb91c290c65c09343eaf53d57fdd5659bf6f2
Balázs Sidlóczki <sidbalazs18@gmail.com>
2024-01-07 15:46:04 +0100

    feat: add conditional rendering for login/logout buttons in DefaultNavbar component

--- Frontend/src/components/DefaultNavbar.jsx
+++ Frontend/src/components/DefaultNavbar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Navbar, Button } from "flowbite-react";
 import { Link, NavLink } from "react-router-dom";
-//import Link
 import "../App.css";
 
 function DefaultNavbar() {
+  const isLoggedIn = localStorage.getItem("accessToken") ? true : false;
   return (
     <Navbar className="bg-navbarBgColor">
       <Navbar.Brand href="/">
@@ -13,15 +13,37 @@ function DefaultNavbar() {
         </span>
       </Navbar.Brand>
       <div className="flex md:order-2 mx-6">
-        <Link to="/login" className="my-2 mx-5 font-medium text-white hover:text-importantText">
-          Log in
-        </Link>
+        {isLoggedIn ? (
+          <>
+            <Link
+              to="/profile"
+              className="my-2 mx-5 font-medium text-white hover:text-importantText"
+            >
+              Dashboard
+            </Link>
+            <Link
+              to="/logout"
+              className="my-2 mx-5 font-medium text-white hover:text-importantText"
+            >
+              Log out
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link
+              to="/login"
+              className="my-2 mx-5 font-medium text-white hover:text-importantText"
+            >
+              Log in
+            </Link>
 
-        <NavLink to="/signup">
-          <Button className="bg-importantText hover:bg-[#564260]">
-            Sign up
-          </Button>
-        </NavLink>
+            <NavLink to="/signup">
+              <Button className="bg-importantText hover:bg-[#564260]">
+                Sign up
+              </Button>
+            </NavLink>
+          </>
+        )}
       </div>
     </Navbar>
   );
