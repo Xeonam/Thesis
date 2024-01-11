c2cced646e08166cf5f89316e20372ad70ad5e98
Balázs Sidlóczki <sidbalazs18@gmail.com>
2024-01-11 12:20:28 +0100

    feat: add next button to flip through flashcards

--- Frontend/src/components/DueCardsComponent.jsx
+++ Frontend/src/components/DueCardsComponent.jsx
@@ -18,10 +18,23 @@ function DueCardsComponent() {
   }
 
   const [isFlipped, setIsFlipped] = useState(false);
+  const [currentCardIndex, setCurrentCardIndex] = useState(0);
 
   const handleClick = () => {
     setIsFlipped(!isFlipped);
   };
+
+  const currentCard = data[currentCardIndex];
+
+  const handleNext = () => {
+    if (currentCardIndex < data.length - 1) {
+      setCurrentCardIndex(currentCardIndex + 1);
+    } else {
+      setCurrentCardIndex(-1); 
+    }
+    setIsFlipped(false);
+  };
+
 
   return (
     <div>
@@ -36,15 +49,17 @@ function DueCardsComponent() {
                   onClick={handleClick}
                   className="p-4 border border-white shadow rounded bg-blue-200"
                 >
-                  {data[0].word.english_word}
+                  {currentCard.word.english_word}
                 </div>
                 <div
                   onClick={handleClick}
                   className="p-4 border border-white shadow rounded bg-blue-100"
                 >
-                  {data[0].word.hungarian_meaning}
+                  {currentCard.word.hungarian_meaning}
                 </div>
               </ReactCardFlip>
+              <button onClick={handleNext} className="mt-4 p-2 bg-blue-500 text-white rounded">Next</button>
+
             </div>
           </div>
         </div>
