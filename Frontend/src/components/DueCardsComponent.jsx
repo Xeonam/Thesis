2c21f2066c0d20695c3c55327bb7035c00a36b5c
Balázs Sidlóczki <sidbalazs18@gmail.com>
2024-01-11 10:00:00 +0100

    feat: fetch and display dueCard components

--- Frontend/src/components/DueCardsComponent.jsx
+++ Frontend/src/components/DueCardsComponent.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { fetchDueCards } from "../api/apiCalls";
 
 function DueCardsComponent() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["dueCards"],
+    queryFn: fetchDueCards,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error! {error.message}</div>;
+  }
   return (
-    <div>DueCards</div>
-  )
+    <div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
 
-export default DueCardsComponent
+export default DueCardsComponent;
