29b5c4a74e32a394d611acab65cdb8796c6442e1
Balázs Sidlóczki <sidbalazs18@gmail.com>
2024-01-11 12:35:11 +0100

    feat:  add counter in DueCardsComponent

--- Frontend/src/components/DueCardsComponent.jsx
+++ Frontend/src/components/DueCardsComponent.jsx
@@ -30,7 +30,7 @@ function DueCardsComponent() {
     if (currentCardIndex < data.length - 1) {
       setCurrentCardIndex(currentCardIndex + 1);
     } else {
-      setCurrentCardIndex(-1); 
+      setCurrentCardIndex(0); 
     }
     setIsFlipped(false);
   };
@@ -44,6 +44,9 @@ function DueCardsComponent() {
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen-90 lg:py-0">
           <div className="w-full bg-[#a7e7c6] rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <div className="text-center mb-4">
+                {currentCardIndex + 1}/{data.length}
+              </div>
               <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
                 <div
                   onClick={handleClick}
