f3f95a3c6d226418a4aaa85a45155ac4b39e8d6f
Balázs Sidlóczki <sidbalazs18@gmail.com>
2024-01-13 18:06:25 +0100

    refactor: update file upload instructions in FileUploadForm.jsx

--- Frontend/src/components/FileUploadForm.jsx
+++ Frontend/src/components/FileUploadForm.jsx
@@ -59,14 +59,28 @@ function FileUploadForm() {
         <div className="fixed inset-0" onClick={toggleModal}>
           <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-blue-200">
             <div className="mt-3 text-center">
-              <h3 className="text-lg leading-6 font-medium text-gray-900">File Upload Instructions</h3>
+              <h3 className="text-lg leading-6 font-medium text-">
+                File Upload Instructions
+              </h3>
               <div className="mt-2 px-7 py-3">
-                <p className="text-sm text-gray-500">
-                  Itt írhatod le a szükséges információkat a txt fájlokról...
+                <p>
+                  Please upload a .txt file containing the words you wish to
+                  learn. The system will automatically translate these words
+                  into Hungarian and add them to your study flashcards.
                 </p>
               </div>
+
+
+              <p className="text-sm font-bold">
+                After the upload is complete, each word from your file will be
+                translated and added to your flashcards for learning.
+              </p>
               <div className="items-center px-4 py-3">
-                <button id="ok-btn" className="px-4 py-2 bg-red-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-purple-300" onClick={toggleModal}>
+                <button
+                  id="ok-btn"
+                  className="px-4 py-2 bg-red-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-purple-300"
+                  onClick={toggleModal}
+                >
                   Close
                 </button>
               </div>
