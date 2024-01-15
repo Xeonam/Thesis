7fa0897fa34d8cc7ba3f8f937f208d2f9bd02dc7
Balázs Sidlóczki <sidbalazs18@gmail.com>
2024-01-15 09:19:01 +0100

    feat: add useQuery hook to fetch decks

--- Frontend/src/components/Decks.jsx
+++ Frontend/src/components/Decks.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import { fetchDecks } from "../api/apiCalls";
 
 function Decks() {
+    const { data, isLoading, error } = useQuery({
+        queryKey: ["decks"],
+        queryFn: fetchDecks,
+      });
+    
+      if (isLoading) {
+        return <div>Loading...</div>;
+      }
+    
+      if (error) {
+        return <div>Error! {error.message}</div>;
+      }
+    
   return (
-    <div>Decks</div>
+    <div className="p-5">
+      Decks
+    </div>
   )
 }
 
