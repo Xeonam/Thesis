377f246b28caef77d807ad73a60adbe82bff110c
Balázs Sidlóczki <sidbalazs18@gmail.com>
2024-01-15 09:19:59 +0100

    refactor: Decks component

--- Frontend/src/components/Decks.jsx
+++ Frontend/src/components/Decks.jsx
@@ -1,26 +1,22 @@
-import React from 'react'
+import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import { fetchDecks } from "../api/apiCalls";
 
 function Decks() {
-    const { data, isLoading, error } = useQuery({
-        queryKey: ["decks"],
-        queryFn: fetchDecks,
-      });
-    
-      if (isLoading) {
-        return <div>Loading...</div>;
-      }
-    
-      if (error) {
-        return <div>Error! {error.message}</div>;
-      }
-    
-  return (
-    <div className="p-5">
-      Decks
-    </div>
-  )
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["decks"],
+    queryFn: fetchDecks,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error! {error.message}</div>;
+  }
+
+  return <div className="p-5">Decks</div>;
 }
 
-export default Decks
+export default Decks;
