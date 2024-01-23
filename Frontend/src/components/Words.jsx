2d9829d20acb88c8d15a444c8f925166c07bae1a
Balázs Sidlóczki <sidbalazs18@gmail.com>
2024-01-23 14:37:58 +0100

    fix: class attribute in Words component

--- Frontend/src/components/Words.jsx
+++ Frontend/src/components/Words.jsx
@@ -28,7 +28,7 @@ function Words() {
               <h3 className="text-lg font-semibold text-gray-800 mb-2">
                 English Word: {wordItem.word.english_word}
               </h3>
-              <hr class="h-px  bg-black border-0"></hr>
+              <hr className="h-px  bg-black border-0"></hr>
               <p className="text-gray-600 pt-2">
                 Hungarian Meaning: {wordItem.word.hungarian_meaning}
               </p>
