bbb15f6243e85f5e6bb9450350aefef044af2c5b
Balázs Sidlóczki <sidbalazs18@gmail.com>
2024-01-26 12:44:32 +0100

    fix: DeckWordPractise component styling

--- Frontend/src/components/DeckWordPractise.jsx
+++ Frontend/src/components/DeckWordPractise.jsx
@@ -89,8 +89,7 @@ function DeckWordPractise() {
     );
   }
   return (
-    <div>
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen-90 lg:py-0">
+      <div className="flex flex-col items-center justify-center overflow-y-auto mt-20">
         <div className="w-full bg-[#a7e7c6] rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <div className="text-center mb-4">
@@ -179,7 +178,6 @@ function DeckWordPractise() {
           </div>
         </div>
       </div>
-    </div>
   );
 }
 
