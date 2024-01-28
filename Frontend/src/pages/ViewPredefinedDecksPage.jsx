695678e418e849c82c1fbe67ca282143d1a4cd22
Balázs Sidlóczki <sidbalazs18@gmail.com>
2024-01-28 09:20:54 +0100

    add: Predefined page

---
+++ Frontend/src/pages/ViewPredefinedDecksPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ViewPredefinedDecksPage, DefaultNavbar } from "../components";
+function ViewPredefinedDecksPage() {
+  return (
+    <div className="bg-[#412A4C] min-h-screen">
+        <DefaultNavbar />
+        <ViewPredefinedDecksPage />
+    </div>
+  )
+}
+
+export default ViewPredefinedDecksPage
