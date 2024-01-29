5242c39435bbfca6074957869807b046d4d951e1
Balázs Sidlóczki <sidbalazs18@gmail.com>
2024-01-29 10:07:49 +0100

    feat: add loading spinner in Words component

--- Frontend/src/components/Words.jsx
+++ Frontend/src/components/Words.jsx
@@ -8,7 +8,11 @@ function Words() {
 
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-purple-600"></div>
+      </div>
+    );
   }
 
   if (error) {
